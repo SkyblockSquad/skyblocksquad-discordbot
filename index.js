198506c0f36960ffa90c14129963e80c51e62385
UltraDeveloper-git <65216982+UltraDeveloper-git@users.noreply.github.com>
2020-05-28 15:36:04 +0200

    init
    init

---
+++ index.js
@@ -0,0 +1,12 @@
+const discord = require("discord.js");
+const botConfig = require("./botconfig.json");
+
+const client = new discord.Client();
+client.login(process.env.token);
+
+client.on("ready", async () => {
+
+    console.log(`${client.user.username} is online.`);
+    client.user.setActivity(",help", {type: "PLAYING"});
+
+});
