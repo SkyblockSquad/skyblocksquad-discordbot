f89a76d80ff7607e5062f04df83427c4e91539ce
UltraDeveloper-git <65216982+UltraDeveloper-git@users.noreply.github.com>
2020-05-30 09:49:07 +0200

    init
    init

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-// Setup
+// Setup discord.js and botconfig.json
 const discord = require("discord.js");
 const botConfig = require("./botconfig.json");
 
-// Variables
+// Setup variables
 var prefix = botConfig.prefix;
 var embedColor = "#ed2121";
+var embedFooter = "SkyblockSquad Bot";
 
-// Login bot
+// Login the bot
 const client = new discord.Client();
 client.login(process.env.token);
 
-// Console log & set activity
+// Console log + set activity
 client.on("ready", async () => {
 
     console.log(`${client.user.username} is online.`);
@@ -32,8 +33,9 @@ client.on("message", async message => {
             .setTitle("HELP")
             .setColor(embedColor)
             .addFields(
-                {name: `${prefix}help`, value: "Display this list"},
-                {name: `${prefix}info`, value: "Display bot and server info"}
+                {name: `${prefix}help`, value: "Display this list!"},
+                {name: `${prefix}info`, value: "Display bot and server info!"},
+                {name: `${prefix}hello`, value: "Say hello to the bot!"}
             )
 
             return message.channel.send(botEmbed);
@@ -41,39 +43,46 @@ client.on("message", async message => {
 
     if(command === `${prefix}info`) {
 
-        var embedTitle = "INFO";
+        var botEmbed = new discord.MessageEmbed()
+        .setTitle("INFO")
+        .setColor(embedColor)
 
         if(!(message.channel.type == "dm")) {
 
-            var botEmbed = new discord.MessageEmbed()
-            .setTitle(embedTitle)
-            .setColor(embedColor)
-            .addFields(
+            botEmbed.addFields(
                 {name: "Server Name", value:message.guild.name},
-                {name: "Server Owner", value: `${message.guild.owner} (${message.guild.ownerID})`},
                 {name: "Server Member Count", value:message.guild.memberCount},
+                {name: "Server Owner", value: `${message.guild.owner} (${message.guild.ownerID})`}
             );            
 
-        } else {
-
-            var botEmbed = new discord.MessageEmbed()
-                .setTitle(embedTitle)
-                .setColor(embedColor)
         }
 
         botEmbed.addFields(
+            {name: "Bot Name", value:client.user},
             {name: "Bot Prefix", value:prefix},
             {name: "Bot Embed Color", value:embedColor},
-            {name: "Bot Uptime", value:client.uptime},
-            {name: "Bot Name", value:client.user}
+            {name: "Bot Uptime", value:client.uptime}
         );
 
             return message.channel.send(botEmbed);
     }
 
+    if(command === `${prefix}me`) {
+
+        var botEmbed = new discord.MessageEmbed()
+            .setTitle("ME")
+            .setColor(embedColor)
+            .addFields(
+                {name: "Your Username", value:message.author.username},
+                {name: "Your Tag", value:message.author.tag},
+                {name: "Your Last Message", value: `${message.author.lastMessage} (${message.author.lastMessageID})`}
+            );
+
+    }
+
     if(command === `${prefix}hello`) {
 
-        return message.channel.send(`Hello there, ${message.user.username}`);
+        return message.channel.send(`Hello there, ${message.author.username}!`);
 
     }
  
