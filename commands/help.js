db985ab4c9acb7dec1c5eefd075fda0a02ae16c0
UltraDeveloper-git <65216982+UltraDeveloper-git@users.noreply.github.com>
2020-10-11 08:08:03 +0200

    init
    init

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,7 @@ module.exports = {
         
         var botEmbed = new discord.MessageEmbed()
         .setTitle("HELP")
-        .setDescription("See a list of bot commands below!\n\n[] means required » {} means optional\nCommands marked with a # are only available in DM!")
+        .setDescription("See a list of bot commands below!\n\n[] means required » {} means optional » () means you have multiple possible arguments\nCommands marked with a # are only available in DM!")
         .setColor(embedColor)
         .setFooter(embedFooter)
         .setTimestamp()
@@ -14,7 +14,8 @@ module.exports = {
             {name: `${prefix}info#`, value: "Display some usefull information!!"},
             {name: `${prefix}me`, value: "Display some usefull information about you!"},
             {name: `${prefix}hack#`, value: "Hack the server!"},
-            {name: `${prefix}is [player] [argument 1] [argment 2]...?#`, value: "Ask anything you want to the bot!\n\n**Minimum 2 arguments - No maximum arguments**"}
+            {name: `${prefix}is [player] [argument 1] [argment 2]...?#`, value: "Ask anything you want to the bot!\n\n**Minimum 2 arguments - No maximum arguments**"},
+            {name: `${prefix}covid (all|[country])`, value: "Get information about the covid-19 virus!\n\n**Minimum 1 argument - Maximum 1 argument**"}
         );
 
         message.channel.send(botEmbed);
