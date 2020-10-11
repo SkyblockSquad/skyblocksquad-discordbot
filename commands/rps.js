6e36613544105774edee3dd4294a1201861e684a
UltraDeveloper-git <65216982+UltraDeveloper-git@users.noreply.github.com>
2020-10-11 07:49:13 +0200

    init
    init

--- commands/rps.js
+++ commands/rps.js
@@ -8,9 +8,9 @@ module.exports = {
         var options = ["rock", "paper", "scissors"];
         var result = options[Math.floor(Math.random() * options.length)];
 
-        if(!args[1].toUpperCase() == "ROCK" && !args[1].toUpperCase() == "PAPER" && !args[1].toUpperCase() == "SCISSORS"]) {
+        if(!args[1].toUpperCase() == "ROCK" && !args[1].toUpperCase() == "PAPER" && !args[1].toUpperCase() == "SCISSORS") {
             
-            message.channel.send("**Error:** Invalid sybtax! Please use: **,rps (rock|paper|scissors)**");
+            message.channel.send("**Error:** Invalid syntax! Please use: **,rps (rock|paper|scissors)**");
             return;
         }
 
