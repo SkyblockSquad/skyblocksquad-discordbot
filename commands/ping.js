0ac6079dc3a858da4e936f091b7d14e3fa808d89
UltraDeveloper-git <65216982+UltraDeveloper-git@users.noreply.github.com>
2020-10-18 09:21:22 +0200

    bugfix
    bugfix

--- commands/ping.js
+++ commands/ping.js
@@ -6,7 +6,7 @@ module.exports = {
         if(args.length >= 2) return message.channel.send("**Error:** No arguments need to be provided!");
         
         message.channel.send(`**Latency:** ${Date.now() - message.createdTimestamp}ms`);
-        message.channel.snd(`**API Latency:** ${Math.round(client.ws.ping)}ms`);
+        message.channel.send(`**API Latency:** ${Math.round(client.ws.ping)}ms`);
 
         return;
 
