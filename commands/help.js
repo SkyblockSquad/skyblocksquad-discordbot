c262db72d4437dc8d11acf8a717b553090020e65
UltraDeveloper-git <65216982+UltraDeveloper-git@users.noreply.github.com>
2020-10-18 09:04:09 +0200

    update
    update

--- commands/help.js
+++ commands/help.js
@@ -2,42 +2,51 @@ module.exports = {
     name: 'help',
     description: 'See a list of all bot commands!',
     category: 'Information',
-    execute(discord, message, prefix, embedColor, embedFooter) {
+    execute(discord, message, prefix, embedColor, embedFooter, args) {
 
-        var botEmbed = new discord.MessageEmbed()
-        .setTitle("HELP")
-        .setDescription("See a list of bot commands below!\n\n[] = required / {} = optional / () = multiple possible arguments\nCommands marked with a # are not available in DM!")
-        .setColor(embedColor)
-        .setFooter(embedFooter)
-        .setTimestamp()
-        .addFields(
-            {name: "\u200b", value: "\u200b"},
-            {name: `:information_source: __Infomation__`, value: "*Get alot of information!*\n**3 Commands**", inline: true},
-            {name: `:tada: __Fun & Games__`, value: "*Play some fun games!*\n**3 Commands**", inline: true},
-            {name: "\u200b", value: "\u200b"},
-            {name: `:microbe: __Covid-19__`, value: "*Eww! The covid-19 virus!*\n**1 Command**"}
-        );
+        if(args.length >= 3) return message.channel.send("**Error:** Invalid syntax! Please use **,help {category}**");
 
-        message.channel.send(botEmbed);
-        return;
-        
-        var botEmbed = new discord.MessageEmbed()
-        .setTitle("HELP")
-        .setDescription("See a list of bot commands below!\n\n[] means required » {} means optional » () means multiple possible arguments\nCommands marked with a # are only available in DM!")
-        .setColor(embedColor)
-        .setFooter(embedFooter)
-        .setTimestamp()
-        .addFields(
-            {name: `${prefix}help`, value: "Display this list!"},
-            {name: `${prefix}info#`, value: "Display some usefull information!!"},
-            {name: `${prefix}me`, value: "Display some usefull information about you!"},
-            {name: `${prefix}hack#`, value: "Hack the server!"},
-            {name: `${prefix}is [player] [argument 1] [argment 2]...?#`, value: "Ask anything you want to the bot!\n\n*Minimum 2 arguments - No maximum arguments*"},
-            {name: `${prefix}covid (all|[country])`, value: "Get information about the covid-19 virus!\n\n*Minimum 1 argument - Maximum 1 argument*"},
-            {name: `${prefix}rps (rock|paper|scissors)`, value: "Play rock, paper, scissors with the bot!\n\n*Minimum 1 argument - Maximum 1 argument*"}
-        );
+        var embedDescription = "\n\n[] = required / {} = optional / () = multiple possible arguments\nCommands marked with a # are not available in DM!";
 
-        message.channel.send(botEmbed);
-        return;
+        if(args.length <= 1) {
+
+            var botEmbed = new discord.MessageEmbed()
+            .setTitle("HELP")
+            .setDescription(`See a list of bot commands below! ${embedDescription}`)
+            .setColor(embedColor)
+            .setFooter(embedFooter)
+            .setTimestamp()
+            .addFields(
+                {name: "\u200b", value: "\u200b"},
+                {name: `:information_source: __Infomation__`, value: "*Get alot of information!*\n**3 Commands**", inline: true},
+                {name: `:tada: __Fun & Games__`, value: "*Play some fun games!*\n**3 Commands**", inline: true},
+                {name: "\u200b", value: "\u200b"},
+                {name: `:microbe: __Covid-19__`, value: "*Eww! The covid-19 virus!*\n**1 Command**"}
+            );
+
+            message.channel.send(botEmbed);
+            return;
+
+        } else if(args.length == 2) {
+
+            if(args[1].toLowerCase() === "information" || args[1].toLowerCase() === "info") {
+                
+                var botEmbed = new discord.MessageEmbed()
+                .setTitle("HELP (INFORMATION)")
+                .setDescription(`See a list of information commands below! ${embedDescription}`)
+                .setColor(embedColor)
+                .setFooter(embedFooter)
+                .setTimestamp()
+                .addFields(
+                    {name: "\u200b", value: "\u200b"},
+                    {name: "__,help {category}__", value: "*See a list of all bot commands\nor see a list of all bot commands from a\nspecific category*"}
+                );
+
+                return message.channel.send(botEmbed);
+
+            }
+
+        } 
+    
     },
 };
