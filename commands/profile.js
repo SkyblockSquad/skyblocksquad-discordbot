ad0dd4843d99390b8e5cd06f47d701fc85aec046
UltraDeveloper-git <65216982+UltraDeveloper-git@users.noreply.github.com>
2020-10-20 18:53:57 +0200

    bugfix

--- commands/profile.js
+++ commands/profile.js
@@ -5,14 +5,18 @@ module.exports = {
     
         if(args.length >= 2 || args.length == 0) return message.channel.send("**Error:** Invalid syntax! Please use **,profile {username}");
 
-        const response = await fetch(`https://api.slothpixel.me/api/skyblock/coins/${args[0]}`);
-        const data = await response.json();
-        const { quick_status } = data;
+        async function getData() {
 
-        let purse = quick_status.purse;
-        let bank = quick_status.bank;
+            const response = await fetch(`https://api.slothpixel.me/api/skyblock/coins/${args[0]}`);
+            const data = await response.json();
+            const { quick_status } = data;
 
-        message.channel.send(`Purse: ${purse} Bank: ${bank}`);
+            let purse = quick_status.purse;
+            let bank = quick_status.bank;
+
+            message.channel.send(`Purse: ${purse} Bank: ${bank}`);
+
+        }
 
     },
 };
