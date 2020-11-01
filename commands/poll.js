fc61f28dc28d8b66bb5983c880426b8239a7b948
UltraDeveloper-git <65216982+UltraDeveloper-git@users.noreply.github.com>
2020-11-01 08:06:57 +0100

    bugfix

--- commands/poll.js
+++ commands/poll.js
@@ -8,7 +8,7 @@ module.exports = {
         var options = message.content.split(" / ");
         var reactions = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣"];
 
-        if(options.length < 3) return message.channel.send("**Error:** Invalid syntax! Please use **,poll [question] [option 1] [option 2] {option 3}...**\n*Minimum 2 options - Maximum 5 options*");
+        if(options.length < 3 || options.length > 6) return message.channel.send("**Error:** Invalid syntax! Please use **,poll [question] [option 1] [option 2] {option 3}...**\n*Minimum 2 options - Maximum 5 options*");
 
         var question = options[0];
         options.shift();
