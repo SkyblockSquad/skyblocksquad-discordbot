5ad2f73891324c5e34612c8e8111f0e9ce8d45e7
UltraDeveloper-git <65216982+UltraDeveloper-git@users.noreply.github.com>
2020-11-02 15:51:12 +0100

    bugfix

--- commands/slowmode.js
+++ commands/slowmode.js
@@ -6,7 +6,9 @@ module.exports = {
         if(!(message.member.hasPermission("MANAGE_CHANNELS"))) return message.channel.send("**Error:** You don't have permission! **(MANAGE_CHANNELS)**");
 
         if(args.length < 1 || args.length > 1) return message.channel.send("**Error:** Invalid syntax! Please use **,slowmode [duration]**");
-        if(!((args[0] / args[0]) === 0)) return message.channel.send(`**Error:** ${args[0]} is not a number!`);
+
+        var slowmode = parseInt(args[0]);
+        if(!((slowmode / slowmode) === 0)) return message.channel.send(`**Error:** ${args[0]} is not a number!`);
 
         message.channel.setRateLimitPerUser(args[0]);
         message.channel.send("**Succes!** The slowmode has been changed! :stopwatch:");
