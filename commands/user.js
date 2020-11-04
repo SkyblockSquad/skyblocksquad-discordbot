39a4d6cb0589650fb631dca4ff2a37e4e661d89d
UltraDeveloper-git <65216982+UltraDeveloper-git@users.noreply.github.com>
2020-11-04 10:57:48 +0100

    bugfix

--- commands/user.js
+++ commands/user.js
@@ -9,7 +9,7 @@ module.exports = {
         if(args.length !== 0) {
             
             var user = args[0];
-            var userGuild = message.guild.members.cache.find(u => u.name === user);
+            var userGuild = message.guild.members.cache.find(u => u.id === user);
 
             if(!(userGuild)) return message.channel.send("**Error:** Oops! Something went wrong!");
 
