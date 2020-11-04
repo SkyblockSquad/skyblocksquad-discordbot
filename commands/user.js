06b1715994604146c79c17e65db60844d1e1bc4e
UltraDeveloper-git <65216982+UltraDeveloper-git@users.noreply.github.com>
2020-11-04 07:39:39 +0100

    patch

--- commands/user.js
+++ commands/user.js
@@ -17,23 +17,20 @@ module.exports = {
             var user = message.author;
         }
 
-        var nickname = user.nickname;
-        if(nickname == null || undefined) nickname = "None";
-
         var botEmbed = new discord.MessageEmbed()
-        .setTitle("ME")
-        .setDescription("See info about you below!")
+        .setTitle(`USER ${member.username.toUpperCase()}`)
+        .setDescription(`See info about ${member.username} below!`)
         .setColor(embedColor)
         .setFooter(embedFooter)
         .setTimestamp()
         .setThumbnail(user.displayAvatarURL({size: 4096}))
         .addFields(
-            {name: "User Name", value:user.username},
-            {name: "User ID", value:user.id},
-            {name: "User Account Created", value: `${moment(user.createdAt).format("LL")}`},
-            {name: "User Status", value:user.presence.status},
-            {name: "User NickName", value:nickname},
-            {name: "User Game", value: `${user.presence.activities[0] ? user.presence.activities[0].name : "None"}`},
+            {name: "User Name", value:user.username, inline: true},
+            {name: "User ID", value:user.id, inline: true},
+            {name: "\u200b", value: "\u200b"},
+            {name: "User Account Created", value: `${moment(user.createdAt).format("LL")}`, inline: true},
+            {name: "User Status", value:user.presence.status, inline: true},
+            {name: "User Game", value: `${user.presence.activities[0] ? user.presence.activities[0].name : "None"}`, inline: true},
         );
 
         return message.channel.send(botEmbed);
