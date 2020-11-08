ed31fe355130420b89c56cc11567d03ea11e2812
UltraDeveloper-git <65216982+UltraDeveloper-git@users.noreply.github.com>
2020-11-08 07:43:13 +0100

    patch

--- commands/apply.js
+++ commands/apply.js
@@ -56,49 +56,59 @@ module.exports = {
                             .setDescription("This is your ticket. Before answering the questions, make sure that you agree with everything in the #applications channel!")
                             .setColor("00BFFF");
 
+                        var question_txt1 = "What is your Minecraft username? (Make sure that all caps are correct and that there are no typo's)";
+                        var question_txt2 = "What is your Discord tag? (For example: **SkyblockSquad Bot#3596**)";
+                        var question_txt3 = "For how long have you been in the guild? (Answer 'IDK' if you don't know, we can check)";
+                        var question_txt4 = "Why would you like to be staff?";
+                        var question_txt5 = "How could you help our staff team?";
+                        var question_txt6 = "Why should we choose you above the other applicants?";
+                        var question_txt7 = "How many hours are online a day on average? (Doesn't have to be every single day, but most of the time)";
+                        var question_txt8 = "How much guild experience do you get a day on average? (Doesn't have to be every single day, but most of the time, you can check using **/guild top**)";
+                        var question_txt9 = "Anything else you want to tell us? (Say **no** if you don't)";
+
                         var question1 = new discord.MessageEmbed()
                             .setTitle("QUESTION 1")
-                            .setDescription("What is your Minecraft username? (Make sure that all caps are correct and that there are no typo's)")
+                            .setDescription(question_txt1)
                             .setColor("00BFFF");
 
                         var question2 = new discord.MessageEmbed()
                             .setTitle("QUESTION 2")
-                            .setDescription(`What is your Discord tag? (For example: **SkyblockSquad Bot#3596**)`)
+                            .setDescription(question_txt2)
                             .setColor("00BFFF");
 
                         var question3 = new discord.MessageEmbed()
                             .setTitle("QUESTION 3")
-                            .setDescription("For how long have you been in the guild? (Answer 'IDK' if you don't know, we can check)")
+                            .setDescription(question_txt3)
                             .setColor("00BFFF");
 
                         var question4 = new discord.MessageEmbed()
                             .setTitle("QUESTION 4")
-                            .setDescription("Why would you like to be staff?")
+                            .setDescription(question_txt4)
                             .setColor("00BFFF");
 
                         var question5 = new discord.MessageEmbed()
                             .setTitle("QUESTION 5")
-                            .setDescription("How could you help our staff team?")
+                            .setDescription(question_txt5)
                             .setColor("00BFFF");
 
                         var question6 = new discord.MessageEmbed()
                             .setTitle("QUESTION 6")
-                            .setDescription("Why should we choose you above the other applicants?")
+                            .setDescription(question_txt6)
                             .setColor("00BFFF");
 
                         var question7 = new discord.MessageEmbed()
                             .setTitle("QUESTION 7")
-                            .setDescription("How many hours are online a day on average? (Doesn't have to be every single day, but most of the time)")
+                            .setDescription(question_txt7)
                             .setColor("00BFFF");
 
                         var question8 = new discord.MessageEmbed()
                             .setTitle("QUESTION 8")
-                            .setDescription("How much guild experience do you get a day on average? (Doesn't have to be every single day, but most of the time, you can check using **/guild top**)")
+                            .setDescription(question_txt8)
                             .setColor("00BFFF");
 
                         var question9 = new discord.MessageEmbed()
                             .setTitle("QUESTION 9")
-                            .setDescription("Anything else you want to tell us? (Say **no** if you don't)")
+                            .setDescription(question_txt9)
                             .setColor("00BFFF");
 
                         settedParent.send(information);
@@ -142,10 +152,21 @@ module.exports = {
 
                                                             var application = new discord.MessageEmbed()
                                                                 .setTitle("THANKS FOR YOUR APPLICATION!")
-                                                                .setDescription(`**Question 1:** ${answer1}\n**Question 2**: ${answer2}\n**Question 3:** ${answer3}\n**Question 4:** ${answer4}\n**Question 5:** ${answer5}\n**Question 6:** ${answer6}\n**Question 7:** ${answer7}\n**Question 8:** ${answer8}\n**Question 9:** ${answer9}`)
+                                                                .setDescription("Thanks for your application!")
                                                                 .setColor("00BFFF")
                                                                 .setFooter(embedFooter)
                                                                 .setTimestamp()
+                                                                .addFields(
+                                                                    {name: `**Question 1:** ${question_txt1}`, value: `Answer: ${answer1}`},
+                                                                    {name: `**Question 2:** ${question_txt2}`, value: `Answer: ${answer2}`},
+                                                                    {name: `**Question 3:** ${question_txt3}`, value: `Answer: ${answer3}`},
+                                                                    {name: `**Question 4:** ${question_txt4}`, value: `Answer: ${answer4}`},
+                                                                    {name: `**Question 5:** ${question_txt5}`, value: `Answer: ${answer5}`},
+                                                                    {name: `**Question 6:** ${question_txt6}`, value: `Answer: ${answer6}`},
+                                                                    {name: `**Question 7:** ${question_txt7}`, value: `Answer: ${answer7}`},
+                                                                    {name: `**Question 8:** ${question_txt8}`, value: `Answer: ${answer8}`},
+                                                                    {name: `**Question 9:** ${question_txt9}`, value: `Answer: ${answer9}`},
+                                                                )
 
                                                             settedParent.bulkDelete(19).then(
                                                                 settedParent.send(application)
@@ -188,7 +209,7 @@ module.exports = {
 
                                                                 var authorEmbed = new discord.MessageEmbed()
                                                                     .setTitle("APPLICATION AUTHOR")
-                                                                    .setDescription(`This application has been send by: <@${author.id}>`)
+                                                                    .setDescription(`This application has been sent by: <@${author.id}>`)
                                                                     .setColor("00BFFF")
                                                                     .setFooter(embedFooter)
 
