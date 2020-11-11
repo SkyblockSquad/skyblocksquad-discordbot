1d489c7a204c0eb6b53a9124e68d1b9140f14a91
UltraDeveloper-git <65216982+UltraDeveloper-git@users.noreply.github.com>
2020-11-11 07:48:28 +0100

    bugfix

--- commands/react.js
+++ commands/react.js
@@ -66,6 +66,7 @@ module.exports = {
 
             setTimeout(function() {
                 embed.edit(editedEmbed);
+                message.channel.send(resultsEmbed);
             }, timeInt);
         }
 
