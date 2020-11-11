07a211f44ed3ad0aa8b8814f7f084dd99221e1d8
UltraDeveloper-git <65216982+UltraDeveloper-git@users.noreply.github.com>
2020-11-11 07:32:41 +0100

    debug

--- commands/react.js
+++ commands/react.js
@@ -51,7 +51,7 @@ module.exports = {
             embed.awaitReactions(filter, { max: amountInt, time: timeInt, errors: ["time"] })
                 .then(collected => {
 
-                    message.channel.send(`**DEBUG:** ${collected.size}`);
+                    message.channel.send(`**DEBUG:** ${collected.size * 2}`);
 
                     if(collected.size > 1) {
                         resultsEmbed.setDescription(`**HOORAY!** We got **${collected.size}** reactions!`);
@@ -83,7 +83,7 @@ module.exports = {
 
                 .catch(collected => {
 
-                    message.channel.send(`**DEBUG:** ${collected.size}`);
+                    message.channel.send(`**DEBUG:** ${collected.size * 2}`);
 
                     if(collected.size > 0) {
                         resultsEmbed.setDescription(`**RIP!** We only got **${collected.size}/${args[1]}** reactions!`);
