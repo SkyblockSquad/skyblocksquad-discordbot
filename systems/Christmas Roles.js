cd79a49987c140d1b373119ad9be242c6746fd8a
UltraDeveloper-git <65216982+UltraDeveloper-git@users.noreply.github.com>
2020-12-06 10:08:02 +0100

    Update Christmas Roles.js

--- systems/Christmas Roles.js
+++ systems/Christmas Roles.js
@@ -8,10 +8,15 @@ module.exports = {
 
         const discord = require("discord.js");
 
-        var snowmanChance = randomChance(20);
-        var santaChance = randomChance(10);
-        var iceWizardChance = randomChance(5);
+        var snowmanChance = randomChance(5);
+        var santaChance = randomChance(5);
+        var iceWizardChance = randomChance(1);
         var yetiChance = randomChance(1);
+
+        var snowmanChance2 = randomChance(5);
+        var santaChance2 = randomChance(5);
+        var iceWizardChance2 = randomChance(1);
+        var yetiChance2 = randomChance(1);
 
         var roleEmbed = new discord.MessageEmbed()
             .setTitle("CHRISTMAS EVENT")
@@ -19,28 +24,28 @@ module.exports = {
             .setColor("#dd2929")
             .setTimestamp()
 
-        if (snowmanChance) {
+        if (snowmanChance && snowmanChance2) {
             var hasSnowman = message.member.roles.cache.has("785071592412807180");
             if (!(hasSnowman)) {
                 message.member.roles.add(message.guild.roles.cache.get("785071592412807180"));
                 roleEmbed.addField("Role", "COMMON! <@&785071592412807180>");
                 message.channel.send(roleEmbed);
             }
-        } else if (santaChance) {
+        } else if (santaChance && santaChance2) {
             var hasSanta = message.member.roles.cache.has("785071932117876736");
             if (!(hasSanta)) {
                 message.member.roles.add(message.guild.roles.cache.get("785071932117876736"));
                 roleEmbed.addField("Role", "RARE! <@&785071932117876736>");
                 message.channel.send(roleEmbed);
             }
-        } else if (iceWizardChance) {
+        } else if (iceWizardChance && iceWizardChance2) {
             var hasIceWizard = message.member.roles.cache.has("785072139882463252");
             if (!(hasIceWizard)) {
                 message.member.roles.add(message.guild.roles.cache.get("785072139882463252"));
                 roleEmbed.addField("Role", "LEGENDARY! <@&785072139882463252>");
                 message.channel.send(roleEmbed);
             }
-        } else if (yetiChance) {
+        } else if (yetiChance && yetiChance2) {
             var hasYeti = message.member.roles.cache.has("785072273295933442");
             if (!(hasYeti)) {
                 message.member.roles.add(message.guild.roles.cache.get("785072273295933442"));
