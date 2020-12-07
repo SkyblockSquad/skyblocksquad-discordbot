060b441253b781f713c809e6b988974d32be6161
SkyblockSquad <65216982+SkyblockSquad@users.noreply.github.com>
2020-12-07 17:30:23 +1000

    Completed apps.js
    Now has capability to open & close

--- commands/apps.js
+++ commands/apps.js
@@ -8,16 +8,25 @@ module.exports = {
     
         var staffManager = message.member.roles.cache.find(role => role.id === "729202553870483498");
         var guildMaster = message.member.roles.cache.find(role => role.id === "683205412488478809");
+        var aArgs = args.join(" ");
         
         if (!staffManager && !guildMaster) {
             return message.channel.send("**Error:**  Insufficient permissions");
         }
         
+        if (aArgs.toLowerCase() === "open") {
+            botConfig.applicationsOpen = true;
+            return true;
+        } else if (aArgs.toLowerCase() === "close") {
+            botConfig.applicationsOpen = false;
+            return false;
+        }
+        
         var embed = new discord.MessageEmbed()
             .setTitle("Application Management")
             .setDescription("Manage applications with the below commands");
-            .setColor(embedColor)
-            .setFooter(embedFooter)
+            .setColor(botConfig.embedColor)
+            .setFooter(botConfig.embedFooter)
             .setTimestamp()
             .addFields(
                 { name: "**,apps open**", value: "Open staff applications" },
