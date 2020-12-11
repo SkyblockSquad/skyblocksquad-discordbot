fbebf7453cb0f5f1963482a82469176126ab79d8
UltraDeveloper-git <65216982+UltraDeveloper-git@users.noreply.github.com>
2020-12-11 15:14:16 +0100

    Create eventresults.js

---
+++ commands/eventresults.js
@@ -0,0 +1,31 @@
+module.exports = {
+    name: 'eventresults',
+    description: 'Display the results of the Christmas Event!',
+    execute(client, message, args) {
+
+        const discord = require("discord.js");
+        const botConfig = require("../data/botconfig.json");
+
+        if (!(message.author.id === "449910090225549312")) return;
+
+        message.delete();
+
+        setTimeout(function() {
+            message.channel.send("**The Christmas Event has ended! The results will appear in 1 minute.**");
+
+            setTimeout(function() {
+                message.channel.send("Event results will appear in **30 seconds**!");
+
+                setTimeout(function() {
+                    var resultsEmbed = new discord.MessageEmbed()
+                        .setTitle("CHRISTMAS EVENT RESULTS")
+                        .setDescription("Below you can find the results for the Christmas Event!\n:first_place: - {score} {name}\n:second_place: - {score} {name}\n:third_place: - {score} {name}")
+                        .setColor(botConfig.embedColor)
+                        .setFooter(botConfig.embedFooter)
+
+                }, 30000);
+            }, 30000)
+        }, 3000);
+
+    },
+};
