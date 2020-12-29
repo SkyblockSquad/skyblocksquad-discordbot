ece5df66273286401bcdd759428d498cf5edb820
SkyblockSquad <65216982+SkyblockSquad@users.noreply.github.com>
2020-12-29 12:24:07 +0100

    Update repeat.js

--- commands/repeat.js
+++ commands/repeat.js
@@ -7,6 +7,8 @@ module.exports = {
         var input = args.join(" ");
 
         if(input.toLowerCase() === "im dumb" || input.toLowerCase() === "i'm dumb") return message.channel.send("Yeah we know.");
+        if(input.toLowerCase() === "im stupid" || input.toLowerCase() === "i'm stupid") return message.channel.send("Yeah we know.");
+        if(input.toLowerCase() === "im an idiot" || input.toLowerCase() === "i'm an idiot") return message.channel.send("Yeah we know.");
 
         message.channel.send(input);
 
