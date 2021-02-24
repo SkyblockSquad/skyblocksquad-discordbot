fcda0de2af7c235ff6cf674dcec766d273380382
SkyblockSquad <65216982+SkyblockSquad@users.noreply.github.com>
2021-02-24 17:09:27 +0100

    Debug

--- systems/Event Roles.js
+++ systems/Event Roles.js
@@ -49,12 +49,12 @@ module.exports = {
         var nextTierChance1 = eventRoles[`tier${currentTier.toString()}`][1];
         var nextTierChance2 = eventRoles[`tier${currentTier.toString()}`][2];
 
-        console.log(isLucky1);
-        console.log(isLucky2);
-
         // Check if the player is lucky
         var isLucky1 = randomChance(nextTierChance1);
         var isLucky2 = randomChance(nextTierChance2);
+
+        console.log(isLucky1);
+        console.log(isLucky2);
 
         // If the player is lucky, level them up
         if (isLucky1 && isLucky2) {
