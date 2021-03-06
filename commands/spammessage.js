a0b624bbd53c233627e84d88369d5932f713da88
SkyblockSquad <65216982+SkyblockSquad@users.noreply.github.com>
2021-03-06 13:49:13 +0100

    [Ultra] Bugfix

--- commands/spammessage.js
+++ commands/spammessage.js
@@ -5,8 +5,7 @@ module.exports = {
     aliases: [],
     execute(client, message, args, isCommand, channel) {
 
-        var run = false;
-        if (!(run)) return message.channel.send("**Error:** This command is currently disabled!");
+        if (permissionLevel(message.member) < 4) return message.channel.send("**Error:** You don't have permission to do this!");
 
         if (args.length < 2) return message.channel.send("**Error:** Invalid syntax! Please use **,spammessage [amount] [message]**!");
 
@@ -16,12 +15,32 @@ module.exports = {
 
         message.delete();
 
-        var messageArray = args.shift();
-        var text = messageArray.join(" ");
+        args.shift();
+
+        var text = args.join(" ");
 
         for (let i = 0; i < amount; i++) {
-
             message.channel.send(text);
+        }
+
+        function permissionLevel(member) {
+
+            var helperRole = member.roles.cache.has("683206050048114728");
+            var moderatorRole = member.roles.cache.has("683205888034603042");
+            var administratorRole = member.roles.cache.has("683205637001183365");
+            var guildMasterRole = member.roles.cache.has("683205412488478809");
+
+            if (!helperRole && !moderatorRole && !administratorRole && !guildMasterRole) {
+                return 1;
+            } else if (helperRole && !moderatorRole && !administratorRole && !guildMasterRole) {
+                return 2;
+            } else if (!helperRole && moderatorRole && !administratorRole && !guildMasterRole) {
+                return 3;
+            } else if (!helperRole && !moderatorRole && administratorRole && !guildMasterRole) {
+                return 4;
+            } else if (!helperRole && !moderatorRole && administratorRole && guildMasterRole) {
+                return 5;
+            }
 
         }
 
